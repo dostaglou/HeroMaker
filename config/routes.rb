071f09bd5656b577d98fb219e6145ff1644725bb
dillon wyatt <dostaglou@gmail.com>
2019-09-16 11:49:13 +0900

    created bio routes, login button for banner

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'biographies/show'
-  get 'biographies/new'
-  get 'biographies/edit'
-  get 'stories/index'
-  get 'stories/show'
-  get 'stories/edit'
   devise_for :users
   root to: 'pages#home'
 
   resources :users, only: [:show]
-  resources :characters
+  resources :characters, except: [:show]
+  resources :characters, only: [:show] do
+    resources :biographies
+  end
   resources :stories
   resources :tables
   post 'dice', to: 'dice#index'
